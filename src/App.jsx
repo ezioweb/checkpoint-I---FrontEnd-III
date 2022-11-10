1e6a49f1e1b402387e3b23bdcfd61b1b081fbaa1
ezioweb <ezioweb@hotmail.com>
2022-11-09 21:04:19 -0300

    código ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,100 @@
-import { Card } from './Card'
+import { useState } from 'react'
+import "./components/style.scss";
+import { Card } from './components/Card'
 
 // Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
+function validacaoNomeDaCor(nomeCor) {
+  return nomeCor.trim().length > 2;
+}
+
+function validacaoCorHexadecimal(corHexadecimal) {
+  // const padraoCorHexadecimal = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+  const padraoCorHexadecimal = /^#(?:[0-9a-f]{3}){1,2}$/i;
+  return padraoCorHexadecimal.test(corHexadecimal);
+}
 
 function App() {
   // Aqui você irá criar os Estados para manipular os Inputs
+  const [nomeCor, setNomeCor] = useState("")
+  const [numeroCor, setNumeroCor] = useState("")
+  const [cores, setCores] = useState([])
+  const [formularioErro, setFormularioErro] = useState(false)
 
+  const cadastrarCor = (event) => {
+    event.preventDefault();
+    let novoId = cores.length 
+    //Validação da cor
+    if (
+      validacaoCorHexadecimal(numeroCor) &&
+      validacaoNomeDaCor(nomeCor)
+    ) {
+      setFormularioErro(false);
 
+      const novaCorCadastrada = {
+        id: novoId,        
+        nome: nomeCor,
+        hexadecimal: numeroCor,
+    };
+    setNomeCor('')
+    setNumeroCor('')
+    setCores([...cores, novaCorCadastrada])
+  } else {
+    setFormularioErro(true);
+  }
+}
   return (
     
    <>
-    <div className="Form">
-       <main className="formInputs">
-     <form>
-     <h1 >Adicionar nova cor</h1>
+    {/* Formulario       */}
+    
+      
+    
+    <div>  
+    <section className='formInputs'>   
+     <header className='tittle-wrapper'>
+     <h1>Adicionar nova cor</h1>
+     </header>
+     <form className={formularioErro ? "form-error" : ""}onSubmit={event => cadastrarCor(event)}>
      <div>
-              <label nomeCor>Nome da cor</label>
+              <label htmlFor='nomeCor'>Nome da cor</label>
               <input
-                id="nomeProduto"
-                type="text"
-                value=""                
+                id='nomeCor'
+                type='text'
+                value={nomeCor}
+                onChange={(event) => setNomeCor(event.target.value)}                
               />
             </div>
             <div>
-              <label precoProduto>Número da cor</label>
+              <label htmlFor='numeroCor'>Cor (hexadecimal)</label>
               <input
-                id="precoProduto"
+                id='numeroCor'
                 type="text"
-                value="Hexadecial"
+                value={numeroCor}
+                onChange={(event) => setNumeroCor(event.target.value)}
                 />
             </div>
-           
-            <button type="submit">Incluir cor</button>
-      {/* Comece a desenvolver o seu Código por aqui :) */}      
-     </form>
-     </main>
+            
+                <button type="submit">Incluir cor</button>
+               
+     </form>  
+     {formularioErro ? <span> Por favor, verifique os dados inseridos no formulário</span> : null}
+     </section>
+     
        </div>
+    <div className='show'>
+      <header className='tittle-wrapper'>
+        <h1>Cores favoritas</h1>
+      </header>
+        <section className='cardsArea'>
+          
+        {cores.map((cor) => (
+          <Card key={cor.id} corData={cor}/>
+        ))}
+        </section>
+    </div>
     
-    <Card />
-   
+  
+  
   </>
     
   )
---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+//Este componente deverá receber dados por Props e mostrar as Informações em Tela
+import "./style.scss";
+
+export function Card(props) {
+  const corFundo = props.corData.hexadecimal
+  return (
+    
+    <div className="cardColor" style={{ backgroundColor: corFundo }}>
+      <div className="cardShow">
+        <h2>{props.corData.nome}</h2>
+        <h3>{props.corData.hexadecimal}</h3>
+      </div>
+    </div>
+  );
+}
